45685e3b1e5e5f1a4fcc0b29b0596e8463cae0fd
Andrew Tomaka <atomaka@gmail.com>
2013-03-05 00:23:46 -0500

    Remove Leftover Stats Testing Driver

--- stats_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  include_once(__DIR__ . '/libs/stats.php');
-
-  $stats = StatsFactory::createStatsObject('http://www.diabloprogress.com/hero/celanian-1548/HsuMing/21706367');
-
-  print_r($stats->class);
-  echo "\n\n";
-  print_r($stats->stats);
-  echo "\n\n";
